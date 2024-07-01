557fc1360d24da61dcc135270364947b5b432efb
Frederick Z <echaya@gmail.com>
2024-07-01 20:38:21 +0800

    vim.g.vscode into a new lua file

--- lua_univ_config.lua
+++ lua_univ_config.lua
@@ -7,10 +7,9 @@ leap.set_default_keymaps()
 require("nvim-surround").setup()
 vim.cmd("highlight NvimSurroundHighlight guifg='#edc41f' gui=underline ctermfg='yellow' cterm=underline")
 
-if vim.g.vscode then
-    vim.keymap.del('x', 'ma')
-    vim.keymap.del('x', 'mi')
-    vim.keymap.del('x', 'mA')
-    vim.keymap.del('x', 'mI')
-end
-
+-- if vim.g.vscode then
+--     vim.keymap.del('x', 'ma')
+--     vim.keymap.del('x', 'mi')
+--     vim.keymap.del('x', 'mA')
+--     vim.keymap.del('x', 'mI')
+-- end
