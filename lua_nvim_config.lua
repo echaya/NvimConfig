639296456450d855f411f8714fff1214cfd5fbd3
Frederick Z <echaya@gmail.com>
2024-07-10 21:04:59 +0800

    nvimtree basic config

--- lua_nvim_config.lua
+++ lua_nvim_config.lua
@@ -291,8 +291,28 @@ require("onedarkpro").setup({
 })
 
 
-require("project_nvim").setup {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+require("project_nvim").setup({})
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+vim.keymap.set("n", "<a-b>", "<CMD>NvimTreeToggle<CR>", {noremap = true, silent = true})
