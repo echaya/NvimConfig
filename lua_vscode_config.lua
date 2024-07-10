e6592c56e9515bdab2a4940aacccb0db0f8aa63e
Frederick Z <echaya@gmail.com>
2024-07-10 19:09:11 +0800

    delete leaplabel

--- lua_vscode_config.lua
+++ lua_vscode_config.lua
@@ -3,9 +3,6 @@ vim.keymap.del('x', 'mi')
 vim.keymap.del('x', 'mA')
 vim.keymap.del('x', 'mI')
 
--- vim.api.nvim_set_hl(0, 'LeapLabel', { fg = '#282c34', bg = '#98c379' })
--- vim.api.nvim_set_hl(0, 'LeapLabelPrimary', { fg = '#dd78c6', bg = '#98c379' })
-
 -- set vim.notify as default notify
 local code = require('vscode')
 vim.notify = code.notify
