e8b535a0d972e6eb85fb6dfacb6c5f16745ad678
Frederick Z. <echaya@gmail.com>
2024-07-20 11:33:30 +0800

    format with stylua

--- lua_vscode_config.lua
+++ lua_vscode_config.lua
@@ -1,8 +1,8 @@
-vim.keymap.del('x', 'ma')
-vim.keymap.del('x', 'mi')
-vim.keymap.del('x', 'mA')
-vim.keymap.del('x', 'mI')
+vim.keymap.del("x", "ma")
+vim.keymap.del("x", "mi")
+vim.keymap.del("x", "mA")
+vim.keymap.del("x", "mI")
 
 -- set vim.notify as default notify
-local code = require('vscode')
+local code = require("vscode")
 vim.notify = code.notify
--- repl_config.lua
+++ repl_config.lua
@@ -1,55 +1,54 @@
-
 local iron = require("iron.core")
 
-iron.setup {
-  config = {
+iron.setup({
+	config = {
 
-    -- Scope of the repl
-    -- By default it is one for the same `pwd`
-    -- Other options are `tab_based` and `singleton`
-    scope = require("iron.scope").path_based,
-    -- Whether a repl should be discarded or not
-    scratch_repl = true,
-    -- Your repl definitions come here
-    repl_definition = {
-        python = {
-            format = require("iron.fts.common").bracketed_paste,
-            command = { "ipython", "--no-autoindent" },
-},
-      sh = {
-        -- Can be a table or a function that
-        -- returns a table (see below)
-        command = {"zsh"}
-      }
-    },
-    -- How the repl window will be displayed
-    -- See below for more information
-    repl_open_cmd = require('iron.view').split.vertical.botright("55%")
-  },
-  -- Iron doesn't set keymaps by default anymore.
-  -- You can set them here or manually add keymaps to the functions in iron.core
-  keymaps = {
-    send_motion = "<Leader>sm",
-    -- send_line = "<Leader>sl",
-    visual_send = "<CR>",
-    send_until_cursor = "<Leader>su",
-    -- send_file = "<Leader>sf",
-    -- send_mark = "<Leader>sm",
-    -- mark_motion = "<Leader>mc",
-    -- mark_visual = "<Leader>mc",
-    -- remove_mark = "<Leader>md",
-    cr = "<CR>",
-    interrupt = "<C-I>",
-    exit = "<Leader>rq",
-    -- clear = "<Leader>cl",
-  },
-  -- If the highlight is on, you can change how it looks
-  -- For the available options, check nvim_set_hl
-  highlight = {
-    italic = true
-  },
-  ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
-}
+		-- Scope of the repl
+		-- By default it is one for the same `pwd`
+		-- Other options are `tab_based` and `singleton`
+		scope = require("iron.scope").path_based,
+		-- Whether a repl should be discarded or not
+		scratch_repl = true,
+		-- Your repl definitions come here
+		repl_definition = {
+			python = {
+				format = require("iron.fts.common").bracketed_paste,
+				command = { "ipython", "--no-autoindent" },
+			},
+			sh = {
+				-- Can be a table or a function that
+				-- returns a table (see below)
+				command = { "zsh" },
+			},
+		},
+		-- How the repl window will be displayed
+		-- See below for more information
+		repl_open_cmd = require("iron.view").split.vertical.botright("55%"),
+	},
+	-- Iron doesn't set keymaps by default anymore.
+	-- You can set them here or manually add keymaps to the functions in iron.core
+	keymaps = {
+		send_motion = "<Leader>sm",
+		-- send_line = "<Leader>sl",
+		visual_send = "<CR>",
+		send_until_cursor = "<Leader>su",
+		-- send_file = "<Leader>sf",
+		-- send_mark = "<Leader>sm",
+		-- mark_motion = "<Leader>mc",
+		-- mark_visual = "<Leader>mc",
+		-- remove_mark = "<Leader>md",
+		cr = "<CR>",
+		interrupt = "<C-I>",
+		exit = "<Leader>rq",
+		-- clear = "<Leader>cl",
+	},
+	-- If the highlight is on, you can change how it looks
+	-- For the available options, check nvim_set_hl
+	highlight = {
+		italic = true,
+	},
+	ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+})
 
 -- iron also has a list of commands, see :h iron-commands for all commands, handled in python.vimrc
 -- vim.keymap.set('n', '<Leader>rr', '<cmd>IronRepl<cr>',{silence=True})
@@ -57,157 +56,166 @@ iron.setup {
 -- vim.keymap.set('n', '<Leader>rh', '<cmd>IronHide<cr>')
 -- vim.keymap.set('n', '<Leader>rf', '<cmd>IronFocus<cr>')
 
-
 require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- Conform will run multiple formatters sequentially
-    python = { "isort", "black" },
-    -- Use a sub-list to run only the first available formatter
-    -- javascript = { { "prettierd", "prettier" } },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- Use a sub-list to run only the first available formatter
+		-- javascript = { { "prettierd", "prettier" } },
+	},
 })
 
 vim.api.nvim_create_user_command("Format", function(args)
-  local range = nil
-  if args.count ~= -1 then
-    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-    range = {
-      start = { args.line1, 0 },
-      ["end"] = { args.line2, end_line:len() },
-    }
-  end
-  require("conform").format({ async = true, lsp_format = "fallback", range = range })
+	local range = nil
+	if args.count ~= -1 then
+		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+		range = {
+			start = { args.line1, 0 },
+			["end"] = { args.line2, end_line:len() },
+		}
+	end
+	require("conform").format({ async = true, lsp_format = "fallback", range = range })
 end, { range = true })
 -- async black call
-vim.keymap.set('n', '==', '<cmd>Format<cr>')
+vim.keymap.set("n", "==", "<cmd>Format<cr>")
 -- sync black call
-vim.keymap.set('n', '<leader>==', ':!black %<cr>')
+vim.keymap.set("n", "<leader>==", ":!black %<cr>")
 
-require('gitsigns').setup{
-  on_attach = function(bufnr)
-    local gitsigns = require('gitsigns')
+require("gitsigns").setup({
+	on_attach = function(bufnr)
+		local gitsigns = require("gitsigns")
 
-    local function map(mode, l, r, opts)
-      opts = opts or {}
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
-    end
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
 
-    -- navigation
-    map('n', 'gj', function()
-      if vim.wo.diff then
-        vim.cmd.normal({']c', bang = true})
-      else
-        gitsigns.nav_hunk('next')
-      end
-    end)
+		-- navigation
+		map("n", "gj", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "]c", bang = true })
+			else
+				gitsigns.nav_hunk("next")
+			end
+		end)
 
-    map('n', 'gk', function()
-      if vim.wo.diff then
-        vim.cmd.normal({'[c', bang = true})
-      else
-        gitsigns.nav_hunk('prev')
-      end
-    end)
+		map("n", "gk", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "[c", bang = true })
+			else
+				gitsigns.nav_hunk("prev")
+			end
+		end)
 
-    -- Actions
-    map('v', 'hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    map('v', 'hu', function() gitsigns.undo_stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    map('n', 'gZ', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    map('v', 'gZ', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    -- map('n', '<leader>hs', gitsigns.stage_hunk)
-    -- map('n', '<leader>hr', gitsigns.reset_hunk)
-    -- map('n', '<leader>hS', gitsigns.stage_buffer)
-    -- map('n', '<leader>hu', gitsigns.undo_stage_hunk)
-    -- map('n', '<leader>hR', gitsigns.reset_buffer)
-    map('n', 'gJ', gitsigns.preview_hunk)
-    -- map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
-    -- map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-    -- map('n', '<leader>hd', gitsigns.diffthis)
-    -- map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
-    map('n', 'gK', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-    map('n', '<leader>td', gitsigns.toggle_deleted)
+		-- Actions
+		map("v", "hs", function()
+			gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end)
+		map("v", "hu", function()
+			gitsigns.undo_stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end)
+		map("n", "gZ", function()
+			gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end)
+		map("v", "gZ", function()
+			gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end)
+		-- map('n', '<leader>hs', gitsigns.stage_hunk)
+		-- map('n', '<leader>hr', gitsigns.reset_hunk)
+		-- map('n', '<leader>hS', gitsigns.stage_buffer)
+		-- map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+		-- map('n', '<leader>hR', gitsigns.reset_buffer)
+		map("n", "gJ", gitsigns.preview_hunk)
+		-- map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
+		-- map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+		-- map('n', '<leader>hd', gitsigns.diffthis)
+		-- map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+		map("n", "gK", '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+		map("n", "<leader>td", gitsigns.toggle_deleted)
 
-    -- Text object
-    -- map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-  end
-}
-
-local hipatterns = require('mini.hipatterns')
-hipatterns.setup({
-  highlighters = {
-    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE', XXX
-    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-    hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
-    todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
-    note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
-
-    -- Highlight hex color strings (`#rrggbb`) using that color
-    hex_color = hipatterns.gen_highlighter.hex_color(),
-  },
+		-- Text object
+		-- map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+	end,
 })
 
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = {"lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+local hipatterns = require("mini.hipatterns")
+hipatterns.setup({
+	highlighters = {
+		-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE', XXX
+		fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+		hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+		todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+		note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+		-- Highlight hex color strings (`#rrggbb`) using that color
+		hex_color = hipatterns.gen_highlighter.hex_color(),
+	},
+})
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = false,
+require("nvim-treesitter.configs").setup({
+	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+	ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
-  -- List of parsers to ignore installing (or "all")
-  ignore_install = { "javascript" ,},
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
 
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = false,
 
-  highlight = {
-    enable = true,
+	-- List of parsers to ignore installing (or "all")
+	ignore_install = { "javascript" },
 
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    -- disable = { "c", "rust", "python" },
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
+	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
+	highlight = {
+		enable = true,
+
+		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+		-- the name of the parser)
+		-- list of language that will be disabled
+		-- disable = { "c", "rust", "python" },
+		-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+		disable = function(lang, buf)
+			local max_filesize = 100 * 1024 -- 100 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
+
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+})
 
 -- default configuration
-require('illuminate').configure({
-    providers = {
-        'lsp',
-        'treesitter',
-    },
-    delay = 100,
-    filetype_overrides = {},
-    filetypes_denylist = {
-        'dirbuf',
-        'dirvish',
-        'fugitive',
-    },
-    filetypes_allowlist = {},
-    modes_denylist = {'i','ic','ix'},
-    under_cursor = true,
-    large_file_cutoff = nil,
-    large_file_overrides = nil,
-    min_count_to_highlight = 2,
-    should_enable = function(bufnr) return true end,
+require("illuminate").configure({
+	providers = {
+		"lsp",
+		"treesitter",
+	},
+	delay = 100,
+	filetype_overrides = {},
+	filetypes_denylist = {
+		"dirbuf",
+		"dirvish",
+		"fugitive",
+	},
+	filetypes_allowlist = {},
+	modes_denylist = { "i", "ic", "ix" },
+	under_cursor = true,
+	large_file_cutoff = nil,
+	large_file_overrides = nil,
+	min_count_to_highlight = 2,
+	should_enable = function(bufnr)
+		return true
+	end,
 })
