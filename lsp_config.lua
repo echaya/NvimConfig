5ef6d7e2a4af1249e11855f55fd652f8e03e93bb
Frederick Z. <echaya@gmail.com>
2024-07-20 11:24:24 +0800

    add mason

--- lsp_config.lua
+++ lsp_config.lua
@@ -198,3 +198,10 @@ vim.diagnostic.config({
 	},
 })
 
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+-- After setting up mason-lspconfig you may set up servers via lspconfig
+-- require("lspconfig").lua_ls.setup {}
+-- require("lspconfig").rust_analyzer.setup {}
+-- ...
