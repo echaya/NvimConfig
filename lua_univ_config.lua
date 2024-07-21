ce765f1f6d31f6cac3eb4977e4c4207847b53509
Frederick Z. <echaya@gmail.com>
2024-07-21 14:20:15 +0800

    add better-escape

--- lua_univ_config.lua
+++ lua_univ_config.lua
@@ -6,3 +6,36 @@ leap.set_default_keymaps()
 
 require("nvim-surround").setup()
 vim.cmd("highlight NvimSurroundHighlight guifg='#edc41f' gui=underline ctermfg='yellow' cterm=underline")
+
+-- require("better_escape").setup {
+--     timeout = vim.o.timeoutlen,
+--     default_mappings = false,
+--     mappings = {
+--         i = {
+--             j = {
+--                 -- These can all also be functions
+--                 k = "<Esc>",
+--             },
+--         },
+--         c = {
+--             j = {
+--                 k = "<Esc>",
+--             },
+--         },
+--         t = {
+--             j = {
+--                 k = "<Esc>",
+--             },
+--         },
+--         v = {
+--             j = {
+--                 k = "<Esc>",
+--             },
+--         },
+--         s = {
+--             j = {
+--                 k = "<Esc>",
+--             },
+--         },
+--     },
+-- }
