26b378319e85aab8d280a6097857ef9edc8af972
Frederick Z. <echaya@gmail.com>
2024-07-23 18:14:13 +0800

    prioritize luasnip over nvim_lsp

--- lsp_config.lua
+++ lsp_config.lua
@@ -59,11 +59,8 @@ cmp.setup({
     }),
 
     sources = cmp.config.sources({
+        { name = "luasnip" }, 
         { name = "nvim_lsp" },
-        -- { name = 'vsnip' }, -- For vsnip users.
-        { name = "luasnip" }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
     }, {
         { name = "buffer" },
     }, {
