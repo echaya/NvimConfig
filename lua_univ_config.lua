63b2cfbfc423a5c7f729d9508197a11c5a63e935
Frederick Z. <echaya@gmail.com>
2024-07-23 16:51:00 +0800

    better up/down using native gj/k

--- lua_univ_config.lua
+++ lua_univ_config.lua
@@ -1,3 +1,7 @@
+-- better up/down
+vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
 local leap = require("leap")
 leap.opts.case_sensitive = true
 leap.set_default_keymaps()
