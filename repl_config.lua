e21cd79a8cb1317c5b281a67bfefb635f57eec5d
Frederick Z. <echaya@gmail.com>
2024-08-11 14:49:37 +0800

    sort treesitter parser

--- repl_config.lua
+++ repl_config.lua
@@ -183,11 +183,12 @@ require("nvim-treesitter.configs").setup({
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   ensure_installed = {
     "lua",
-    "vim",
-    "vimdoc",
-    "query",
     "markdown",
     "markdown_inline",
+    "python",
+    "query",
+    "vim",
+    "vimdoc",
   },
   sync_install = false,
   auto_install = false,
