24bdc5ded2884c735549deabbf22fadf3a095690
Frederick Z. <echaya@gmail.com>
2024-08-17 14:52:26 +0800

    remove unused options from lsp

--- lsp_config.lua
+++ lsp_config.lua
@@ -4,25 +4,19 @@ local luasnip = require("luasnip")
 
 cmp.setup({
   snippet = {
-    -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
       require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-      -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
     end,
   },
   window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
-    -- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         if luasnip.expandable() then
@@ -68,18 +62,6 @@ cmp.setup({
   }),
 })
 
--- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
--- Set configuration for specific filetype.
---[[ cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-        { name = 'git' },
-    }, {
-        { name = 'buffer' },
-    })
-})
-require("cmp_git").setup() ]]
---
-
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ "/", "?" }, {
   mapping = cmp.mapping.preset.cmdline(),
@@ -99,29 +81,22 @@ cmp.setup.cmdline(":", {
   matching = { disallow_symbol_nonprefix_matching = false },
 })
 
--- Set up lspconfig.
--- local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
--- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {capabilities = capabilities}
-
 -- Setup luasnip
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip").filetype_extend("vimwiki", { "markdown" })
--- require("luasnip.loaders.from_vscode").load({ include = {"markdown","md"} })
--- vim.keymap.set({"i"}, "<CR>", function() ls.expand() end, {silent = true})
 luasnip.config.set_config({
   region_check_events = "InsertEnter",
   delete_check_events = "InsertLeave",
 })
 
 -- Setup Autocomplete
-
 require("nvim-autopairs").setup({
   disable_filetype = { "TelescopePrompt", "NvimTree", "oil" },
 })
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
+-- Setup LSP
 local lsp = require("lspconfig")
 local navic = require("nvim-navic")
 
