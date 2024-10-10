4f7d35fab3c11ed9e5165874919650dc11fa23f3
Frederick Z. <34254408+echaya@users.noreply.github.com>
2024-10-10 17:38:34 +0800

    noice notification for macro recording

--- nvim_gui_config.lua
+++ nvim_gui_config.lua
@@ -400,3 +400,26 @@ require("noice").setup({
     lsp_doc_border = false, -- add a border to hover docs and signature help
   },
 })
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  callback = function()
+    local msg = string.format("Register:  %s", vim.fn.reg_recording())
+    _MACRO_RECORDING_STATUS = true
+    vim.notify(msg, vim.log.levels.INFO, {
+      title = "Macro Recording",
+      keep = function() return _MACRO_RECORDING_STATUS end,
+    })
+  end,
+  group = vim.api.nvim_create_augroup("NoiceMacroNotfication", {clear = true})
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+  callback = function()
+    _MACRO_RECORDING_STATUS = false
+    vim.notify("Success!", vim.log.levels.INFO, {
+      title = "Macro Recording End",
+      timeout = 2000,
+    })
+  end,
+  group = vim.api.nvim_create_augroup("NoiceMacroNotficationDismiss", {clear = true})
+})
