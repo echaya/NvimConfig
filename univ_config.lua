8fdc85eded2b6cfa2ae00c38c38f3d005bd15c9b
Frederick Z. <echaya@gmail.com>
2024-11-23 15:15:49 +0800

    <localleader>s to highlight surround

--- univ_config.lua
+++ univ_config.lua
@@ -87,15 +87,15 @@ require("mini.ai").setup({
   },
 })
 
-require('mini.surround').setup({
-    mappings = {
-      add = 'ys',
-      delete = 'ds',
-      find = '',
-      find_left = '',
-      highlight = '',
-      replace = 'cs',
-      update_n_lines = '',
+require("mini.surround").setup({
+  mappings = {
+    add = "ys",
+    delete = "ds",
+    find = "",
+    find_left = "",
+    highlight = '<localleader>s',
+    replace = "cs",
+    update_n_lines = "",
 
       -- Add this only if you don't want to use extended mappings
       suffix_last = '',
