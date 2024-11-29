94726a2cb8d49e4ee302563b4374e2c484dbdcc1
Frederick Z. <echaya@gmail.com>
2024-11-29 21:08:05 +0800

    move dropbox to tools

--- copy_linux_treesitter.sh
+++ copy_linux_treesitter.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-cp -r ../plugged/nvim-treesitter/parser/ /mnt/d/Dropbox/neovim/plugged/linux-treesitter/
-cp -r ../plugged/nvim-treesitter/parser-info/ /mnt/d/Dropbox/neovim/plugged/linux-treesitter/
-rm /mnt/d/Dropbox/neovim/plugged/linux-treesitter/parser/.gitignore
-rm /mnt/d/Dropbox/neovim/plugged/linux-treesitter/parser-info/.gitignore
+cp -r ../plugged/nvim-treesitter/parser/ /mnt/c/tools/neovim/plugged/linux-treesitter/
+cp -r ../plugged/nvim-treesitter/parser-info/ /mnt/c/tools/neovim/plugged/linux-treesitter/
+rm /mnt/c/tools/neovim/plugged/linux-treesitter/parser/.gitignore
+rm /mnt/c/tools/neovim/plugged/linux-treesitter/parser-info/.gitignore
