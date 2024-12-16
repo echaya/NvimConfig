ae0ac0d12a0c90455d00290500925b8c8eecb137
Frederick Z. <echaya@gmail.com>
2024-12-16 10:33:35 +0800

    enhance session auto save copying from session-manager nofile check

--- nvim_gui_config.lua
+++ nvim_gui_config.lua
@@ -486,6 +486,12 @@ mini_session.setup({
 -- auto save session
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   callback = function()
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      -- Don't save while there's any 'nofile' buffer open.
+      if vim.api.nvim_get_option_value("buftype", { buf = buf }) == "nofile" then
+        return
+      end
+    end
     local cwd = vim.fn.getcwd()
     cwd = cwd:gsub("[:/\\]$", ""):gsub(":", ""):gsub("[/\\]", "_")
     mini_session.write(cwd)
