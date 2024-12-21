e5180effeafbedffe1a92afdbf1b824ee66a541b
Frederick Z. <echaya@gmail.com>
2024-12-21 20:46:54 +0800

    telescope qol c-j/k to navigate files

--- nvim_utils_config.lua
+++ nvim_utils_config.lua
@@ -43,8 +43,10 @@ telescope.setup({
     path_display = { "truncate" },
     mappings = {
       i = {
-        ["<C-j>"] = actions.cycle_history_next,
-        ["<C-k>"] = actions.cycle_history_prev,
+        ["<C-n>"] = actions.cycle_history_next,
+        ["<C-p>"] = actions.cycle_history_prev,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-j>"] = actions.move_selection_next,
         ["<C-x>"] = false,
         ["<C-s>"] = actions.select_horizontal,
         ["<CR>"] = select_one_or_multi,
