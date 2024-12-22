aa9372a48900fdb183ba5162bd319eea0bc3cd78
Frederick Z. <echaya@gmail.com>
2024-12-22 13:50:38 +0800

    copy blink so
    rename

--- copy_linux_treesitter.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-cp -r ../plugged/nvim-treesitter/parser/ /mnt/c/tools/neovim/plugged/linux-treesitter/
-cp -r ../plugged/nvim-treesitter/parser-info/ /mnt/c/tools/neovim/plugged/linux-treesitter/
-rm /mnt/c/tools/neovim/plugged/linux-treesitter/parser/.gitignore
-rm /mnt/c/tools/neovim/plugged/linux-treesitter/parser-info/.gitignore
