304d8ab2f535b80b377d9ecfba988aa782a17ad1
Frederick Z. <echaya@gmail.com>
2025-01-03 20:52:24 +0800

    update copy_so

--- copy_linux_treesitter.sh
+++ copy_linux_treesitter.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-cp -r ../plugged/nvim-treesitter/parser/ /mnt/c/tools/neovim/plugged/linux-treesitter/
-cp -r ../plugged/nvim-treesitter/parser-info/ /mnt/c/tools/neovim/plugged/linux-treesitter/
-rm /mnt/c/tools/neovim/plugged/linux-treesitter/parser/.gitignore
-rm /mnt/c/tools/neovim/plugged/linux-treesitter/parser-info/.gitignore
+# cp -r ../plugged/nvim-treesitter/parser/ /mnt/c/tools/neovim/plugged/linux-treesitter/
+# cp -r ../plugged/nvim-treesitter/parser-info/ /mnt/c/tools/neovim/plugged/linux-treesitter/
+# rm /mnt/c/tools/neovim/plugged/linux-treesitter/parser/.gitignore
+# rm /mnt/c/tools/neovim/plugged/linux-treesitter/parser-info/.gitignore
+cp /home/z/.local/share/nvim/site/pack/deps/opt/blink.cmp/target/release/libblink_cmp_fuzzy.so /mnt/c/Users/echay/AppData/Local/nvim-data/site/pack/deps/opt/blink.cmp/target/release/
