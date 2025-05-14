c9fc317785b6fe096ed85fd7030b8e1d9a68b293
Frederick Z. <echaya@gmail.com>
2025-05-14 19:03:07 +0800

    support flash in snacks.pcker using <a-m>

--- nvim_now_config.lua
+++ nvim_now_config.lua
@@ -145,6 +145,7 @@ Snacks.setup({
           ["/"] = false,
           ["<c-n>"] = false,
           ["<c-p>"] = false,
+          ["<a-m>"] = { "flash", mode = { "n", "i" } },
           ["<a-l>"] = { "toggle_focus", mode = { "i", "n" } },
         },
       },
@@ -171,6 +172,26 @@ Snacks.setup({
           ["<a-w>"] = "cycle_win",
         },
       },
+    },
+    actions = {
+      flash = function(picker)
+        require("flash").jump({
+          pattern = "^",
+          label = { after = { 0, 0 } },
+          search = {
+            mode = "search",
+            exclude = {
+              function(win)
+                return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
+              end,
+            },
+          },
+          action = function(match)
+            local idx = picker.list:row2idx(match.pos[1])
+            picker.list:_move(idx, true, true)
+          end,
+        })
+      end,
     },
     db = {
       sqlite3_path = "c:/tools/CliTools/sqlite3.dll", ---@type string?
