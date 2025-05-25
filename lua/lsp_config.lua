ed8b0e0b74467677b6098c4355c49533b57003df
Frederick Z. <echaya@gmail.com>
2025-05-26 06:46:52 +0800

    sort parser

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -2,17 +2,17 @@
 require("nvim-treesitter.configs").setup({
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   ensure_installed = {
+    "bash",
+    "gitcommit",
     "lua",
     "markdown",
     "markdown_inline",
+    "powershell",
     "python",
     "query",
+    "regex",
     "vim",
     "vimdoc",
-    "bash",
-    "powershell",
-    "gitcommit",
-    "regex",
   },
   sync_install = false,
   auto_install = true,
