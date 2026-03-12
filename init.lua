7bac20f2f0ab91b76e423ff35ea7605ec0a6a75b
Frederick Z. <echaya@gmail.com>
2026-03-12 19:19:13 +0800

    remove a comment

--- init.lua
+++ init.lua
@@ -86,7 +86,7 @@ if vim.g.vscode == nil then
       depends = {
         "echaya/friendly-snippets",
       },
-      checkout = "v1.8.0", -- check releases for latest tag
+      checkout = "v1.8.0",
     })
     add({
       source = "nvim-treesitter/nvim-treesitter",
